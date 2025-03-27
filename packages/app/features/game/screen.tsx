cd8833eb087d4df47daa9f24f37a698a9a127bba
Guysnacho <tunjiprod@gmail.com>
2025-03-27 01:17:58 -0500

    Add routing from match history to game

--- packages/app/features/game/screen.tsx
+++ packages/app/features/game/screen.tsx
@@ -1,4 +1,4 @@
-import { Button, H3, H5, Paragraph, ScrollView, XStack, YStack } from '@my/ui'
+import { Button, Card, H3, H5, Paragraph, ScrollView, XStack, YStack } from '@my/ui'
 import { ChevronLeft } from '@tamagui/lucide-icons'
 import { GameState, useGames } from 'app/store'
 import { useEffect, useState } from 'react'
@@ -26,39 +26,7 @@ export function GameScreen() {
       <ScrollView maxH="80%" width="80%" $md={{ width: '50%' }} $lg={{ width: '35%' }} mx="auto">
         <YStack gap="$5">
           {games && games.length > 0 ? (
-            games.map((game, idx) => (
-              <YStack>
-                <XStack key={game.id} mx="auto" gap="$5">
-                  <H5 size="$7" my="auto">
-                    #{idx}
-                  </H5>
-                  <YStack>
-                    <XStack gap="$5">
-                      <H5>Team A</H5>
-                      {game.teamA.playerB ? (
-                        <Paragraph>
-                          {game.teamA.playerA.points! + game.teamA.playerB.points! + ' Points'}
-                        </Paragraph>
-                      ) : (
-                        <Paragraph>{game.teamA.playerA.points! + ' Points'}</Paragraph>
-                      )}
-                    </XStack>
-
-                    <XStack gap="$5">
-                      <H5>Team B</H5>
-                      {game.teamB.playerB ? (
-                        <Paragraph>
-                          {game.teamB.playerA.points! + game.teamB.playerB.points! + ' Points'}
-                        </Paragraph>
-                      ) : (
-                        <Paragraph>{game.teamB.playerA.points! + ' Points'}</Paragraph>
-                      )}
-                    </XStack>
-                  </YStack>
-                </XStack>
-                <Paragraph mx="auto">{game.createdAt.toISOString()}</Paragraph>
-              </YStack>
-            ))
+            games.map((game, idx) => <GameCard key={idx} index={idx} game={game} router={router} />)
           ) : (
             <></>
           )}
@@ -68,5 +36,53 @@ export function GameScreen() {
         Go Home
       </Button>
     </YStack>
+  )
+}
+
+const GameCard = ({
+  game,
+  index,
+  router,
+}: {
+  game: GameState['games'][string]
+  index: number
+  router: { push: (path: string) => void }
+}) => {
+  return (
+    <Card onPress={() => router.push('/game/' + game.id)}>
+      <Card.Header>
+        <YStack>
+          <XStack key={game.id} mx="auto" gap="$5">
+            <H5 size="$7" my="auto">
+              #{index}
+            </H5>
+            <YStack>
+              <XStack gap="$5">
+                <H5>Team A</H5>
+                {game.teamA.playerB ? (
+                  <Paragraph>
+                    {game.teamA.playerA.points! + game.teamA.playerB.points! + ' Points'}
+                  </Paragraph>
+                ) : (
+                  <Paragraph>{game.teamA.playerA.points! + ' Points'}</Paragraph>
+                )}
+              </XStack>
+
+              <XStack gap="$5">
+                <H5>Team B</H5>
+                {game.teamB.playerB ? (
+                  <Paragraph>
+                    {game.teamB.playerA.points! + game.teamB.playerB.points! + ' Points'}
+                  </Paragraph>
+                ) : (
+                  <Paragraph>{game.teamB.playerA.points! + ' Points'}</Paragraph>
+                )}
+              </XStack>
+            </YStack>
+          </XStack>
+          <Paragraph mx="auto">{game.createdAt.toISOString()}</Paragraph>
+        </YStack>
+      </Card.Header>
+    </Card>
   )
 }
