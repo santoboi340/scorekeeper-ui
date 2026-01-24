47bfa240b9acb924ff38f29f1bb7aefe22603da9
Terrance <terrancem.brown@gmail.com>
2026-01-24 12:46:10 -0600

    Fixing String errors

--- src/components/Profile/ProfileView.tsx
+++ src/components/Profile/ProfileView.tsx
@@ -8,7 +8,7 @@ interface ProfileViewProps {
 }
 
 export default function ProfileView({ profile, onEdit }: ProfileViewProps) {
-    const token = sessionStorage.getItem('access_token')
+    const token = sessionStorage.getItem('access_token') || ''
     const payload = JSON.parse(atob(token.split('.')[1]))
     console.log(payload)
     console.log('The Above Console log is located in ProfileView.tsx')
