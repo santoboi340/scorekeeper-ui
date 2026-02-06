d71d38aa7ce63500998ca69647d3733ad5bc6dc5
Magus Chihaba <chihabam@gmail.com>
2026-02-05 22:50:33 -0600

    added the right env variable property

--- globalVar.tsx
+++ globalVar.tsx
@@ -1,4 +1,4 @@
-export const API_URL = (process.env.WEB_ORIGIN !== undefined) ? `${process.env.WEB_ORIGIN}` : "https://scorepal-dev.mts-lab.net";
+export const API_URL = (process.env.NEXT_PUBLIC_WEB_ORIGIN !== undefined) ? `${process.env.NEXT_PUBLIC_WEB_ORIGIN}` : "https://scorepal-dev.mts-lab.net";
 
 // if(API_URL===undefined)
 // API_URL = `http://${window.location.hostname}:8080`
